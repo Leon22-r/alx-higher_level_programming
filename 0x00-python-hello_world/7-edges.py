e74353862c93bc3636c68e1b9fc9f48ff3f38af8
Leon22-r <121898166+Leon22-r@users.noreply.github.com>
2023-08-23 21:52:20 +0300

    correcting pycodestyle

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3] #prints the first three letters
-word_last_2 = word[-2:] #prints last 2 letters
-middle_word = word[1:-1] #eliminates the first and last letters
+word_first_3 = word[:3]  # prints the first three letters
+word_last_2 = word[-2:]  # prints last 2 letters
+middle_word = word[1:-1]  # eliminates the first and last letters
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
